4bbcffdba5886cb6919d2e28616461dfcba80d4b
juancp13 <juanillo.133@gmail.com>
2022-10-28 23:41:54 +0200

    Adecuación página

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -71,8 +71,8 @@ const config = {
           },
           {to: '/blog', label: 'Blog', position: 'left'},
           {
-            href: 'https://github.com/facebook/docusaurus',
-            label: 'GitHub',
+            href: 'https://github.com/Juancp13/EscLibrary',
+            src: 'img/GitHub-Mark-32px.png',
             position: 'right',
           },
         ],
