c6b2de0b15fd388ac0dc5e57b5e7a876f3afcb40
juancp13 <juanillo.133@gmail.com>
2022-10-31 02:56:38 +0100

    Cambio github footer

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -115,7 +115,7 @@ const config = {
               },
               {
                 label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
+                href: 'https://github.com/Juancp13/EscLibrary',
               },
             ],
           },
