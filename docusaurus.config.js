3f57dd2ec96c30e5e01bf76ba455f67ece86baac
juancp13 <juanillo.133@gmail.com>
2022-10-31 02:18:40 +0100

    Edit link

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -37,14 +37,14 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/Juancp13/EscLibrary',
+            'https://github.com/Juancp13/EscLibrary/tree/main',
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/Juancp13/EscLibrary',
+            'https://github.com/Juancp13/EscLibrary/tree/main',
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
